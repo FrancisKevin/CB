f1714449765716102bd866e448bfac25b503b13e
FrancisKevin <chen331339299@163.com>
2015-03-02 14:07:15 +0800

    加入CollectionView

---
+++ CBCollectionView/CBCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CBCollectionView
+//
+//  Created by Bing on 15/3/2.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
